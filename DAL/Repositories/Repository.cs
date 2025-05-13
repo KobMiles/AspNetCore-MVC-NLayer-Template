4283daa7e4938836fa65281ec17b495ed598d711
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-05-13 20:48:31 +0300

    Refactor Repository to implement IEntityRepository
    Updated the `Repository<TEntity>` class in `Repository.cs` to implement `IEntityRepository<TEntity>` instead of `IRepository<TEntity>`. This change reflects a new design in the data access layer, focusing on entity-specific operations.

--- DAL/Repositories/Repository.cs
+++ DAL/Repositories/Repository.cs
@@ -6,7 +6,7 @@ using DAL.Interfaces.Repositories;
 namespace DAL.Repositories;
 
 public class Repository<TEntity>(DbContext context)
-    : RepositoryBase<TEntity>(context), IRepository<TEntity>
+    : RepositoryBase<TEntity>(context), IEntityRepository<TEntity>
     where TEntity : class, IEntity
 {
 

0cf49f136da443a43b0d4de1e91892581d1116ca
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-05-13 20:47:50 +0300

    Add UserByIdReadOnlySpec and remove UserByIdSpec
    Introduces a new specification class `UserByIdReadOnlySpec` for retrieving users by ID with a no-tracking query. Removes the existing `UserByIdSpec` class, which lacked the no-tracking functionality.

---
+++ DAL/Entities/Specifications/Users/UserByIdReadOnlySpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace DAL.Entities.Specifications.Users;
+
+public sealed class UserByIdReadOnlySpec : Specification<User>
+{
+    public UserByIdReadOnlySpec(string userId)
+    {
+        Query.Where(u => u.Id == userId)
+            .AsNoTracking();
+    }
+}

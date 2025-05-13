510032fe5dbea6cdef972228e07874a38fea1bd4
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-05-13 20:47:40 +0300

    Update UserConfiguration for entity properties
    Changed namespace to DAL.Entities.Configurations.Identity.
    Added required configuration for FirstName and LastName
    with a maximum length of 100 characters each.

--- DAL/Entities/Configurations/Identity/UserConfiguration.cs
+++ DAL/Entities/Configurations/Identity/UserConfiguration.cs
@@ -8,5 +8,15 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.ToTable("Users");
+
+        builder
+            .Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder
+            .Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
     }
 }

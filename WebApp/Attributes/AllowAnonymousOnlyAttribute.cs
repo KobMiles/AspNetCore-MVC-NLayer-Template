22b3903c326306e41ec4e04f3ba8b2ee636e51fa
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-05-14 00:03:03 +0300

    Add AllowAnonymousOnlyAttribute for user access control
    Introduces `AllowAnonymousOnlyAttribute` class that inherits from `ActionFilterAttribute`. This attribute checks if a user is authenticated and redirects them to the "Index" action of the "Home" controller if they are. It is designed to restrict access to certain actions or controllers, allowing only anonymous users.

---
+++ WebApp/Attributes/AllowAnonymousOnlyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Attributes;
+
+public class AllowAnonymousOnlyAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var user = context.HttpContext.User;
+
+        if (user.Identity?.IsAuthenticated == true)
+        {
+            context.Result = new RedirectToActionResult("Index", "Home", null);
+        }
+
+        base.OnActionExecuting(context);
+    }
+}
